9b1f04e3f473cbb46cdbe21e4b467851d8e18344
Markus Bucher <bucher@atix.de>
2021-05-12 11:17:46 +0200

    Fix test-script problems with large files

--- expected/debversion_test.py
+++ expected/debversion_test.py
@@ -1,14 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # # This file generates outputfile for debversioncompare tests using the built in version compare from dpkg.
 
 from subprocess import run
 import re
+import apt_pkg
 
-def compare(version1, version2, op):
+FILES=[ 'debversion_test', 'test_case_from_dpkg', 'brute-force-test' ]
+
+apt_pkg.init_system()
+
+def compare_lib(version1, version2, op):
+    res = apt_pkg.version_compare(version1, version2)
+
+    if (res < 0):
+        if (op == '<' or op == '<=' or op == '!='):
+            return 't'
+        else:
+            return 'f'
+    elif (res == 0):
+        if (op == '<=' or op == '>=' or op == '='):
+            return 't'
+        else:
+            return 'f'
+    elif (res > 0):
+        if (op == '>' or op == '>=' or op == '!='):
+            return 't'
+        else:
+            return 'f'
+    else:
+        raise "Unsupported combination ver1=%s ver2=%s operator=%s" % (version1, version2, op)
+
+def compare_cmd(version1, version2, op):
     dictionary = {"<":"lt", ">":"gt", "<=":"le", ">=":"ge", "=":"eq", "!=":"ne"}
-    dpkg_op = dictionary[op] 
+    dpkg_op = dictionary[op]
     command = f"dpkg --compare-versions {version1} {dpkg_op} {version2}"
     result = run(command.split())
     if result.returncode == 0:
@@ -16,47 +42,32 @@ def compare(version1, version2, op):
     else:
         return 'f'
 
-with open("../sql/debversion_test.sql") as f:
-    fin = f.readlines()
+def create_output(line):
+    mask = re.compile(r"SELECT '([0-9a-zA-Z\+\-\.\:\~]+)'::debversion ([\>\<\=\!]+) '([0-9a-zA-Z\+\-\.\:\~]+)'::debversion;")
+    match = mask.match(line)
+    version1 = match.group(1)
+    op = match.group(2)
+    version2 = match.group(3)
+    letter = compare(version1, version2, op)
+    return f" ?column? \n----------\n {letter}\n(1 row)\n\n"
 
-output = open("debversion_test.out", "wt")
-for line in fin:
-    if line == '\n':
-        continue
-    output.write(line)
-    if line.startswith("SELECT '1:bla-1'::debversion"):
-    	output.write("WARNING:  version 1:bla-1 has bad syntax: version number does not start with digit\n")
-    if line.startswith("SELECT"):
-        mask = re.compile(r"SELECT '([0-9a-zA-Z\+\-\.\:\~]+)'::debversion ([\>\<\=\!]+) '([0-9a-zA-Z\+\-\.\:\~]+)'::debversion;")
-        match = mask.match(line)
-        version1 = match.group(1)
-        op = match.group(2)
-        version2 = match.group(3)
-        letter = compare(version1, version2, op)
-        text = f" ?column? \n----------\n {letter}\n(1 row)\n\n"
-        output.write(text)
+# CHANGEME to use apt-pkg library or dpkg command for comparing
+compare = compare_lib
 
-output.close()
-
-
-
-with open("../sql/test_case_from_dpkg.sql") as f:
-    fin = f.readlines()
-
-output = open("test_case_from_dpkg.out", "wt")
-for line in fin:
-    if line == '\n':
-        continue
-    output.write(line)
-    if line.startswith("SELECT"):
-        mask = re.compile(r"SELECT '([0-9a-zA-Z\+\-\.\:\~]+)'::debversion ([\>\<\=\!]+) '([0-9a-zA-Z\+\-\.\:\~]+)'::debversion;")
-        match = mask.match(line)
-        version1 = match.group(1)
-        op = match.group(2)
-        version2 = match.group(3)
-        letter = compare(version1, version2, op)
-        text = f" ?column? \n----------\n {letter}\n(1 row)\n\n"
-        output.write(text)
-
-output.close()
-
+for filename in FILES:
+    print(f"Processing {filename}...", end='')
+    try:
+        with open(f"../sql/{filename}.sql") as fin:
+            output = open(f"{filename}.out", "wt")
+            for line in fin:
+                if line == '\n':
+                    continue
+                output.write(line)
+                if line.startswith("SELECT '1:bla-1'::debversion"):
+                  output.write("WARNING:  version 1:bla-1 has bad syntax: version number does not start with digit\n")
+                if line.startswith("SELECT"):
+                    output.write(create_output(line))
+            output.close()
+        print('done')
+    except IOError:
+        print("File not accessible")
--- sql/test_dpkg.py
+++ sql/test_dpkg.py
@@ -1,24 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # # This file generates "SELECT" test statements for dpkg test cases
 
-with open("../sql/test_dpkg") as f:
-    fin = f.readlines()
+with open("test_dpkg") as f:
+    output = open("test_case_from_dpkg.sql", "wt")
+    for line in f:
+        if line == '\n':
+            continue
+        data = line.split()
+        version1 = data[0]
+        version2 = data[1]
+        text = f"SELECT '{version1}'::debversion < '{version2}'::debversion;\n"
+        output.write(text)
+        text = f"SELECT '{version1}'::debversion > '{version2}'::debversion;\n"
+        output.write(text)
+        text = f"SELECT '{version1}'::debversion = '{version2}'::debversion;\n"
+        output.write(text)
 
-output = open("test_case_from_dpkg.sql", "wt")
-for line in fin:
-    if line == '\n':
-        continue
-    data = line.split()
-    version1 = data[0]
-    version2 = data[1]
-    text = f"SELECT '{version1}'::debversion < '{version2}'::debversion;\n"
-    output.write(text)
-    text = f"SELECT '{version1}'::debversion > '{version2}'::debversion;\n"
-    output.write(text)
-    text = f"SELECT '{version1}'::debversion = '{version2}'::debversion;\n"
-    output.write(text)
-
-output.close()
-
+    output.close()
